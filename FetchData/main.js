298dcff6a2f27c38ac355035555e7f90b4ab3241
mannycolon <colonmanuel7@gmail.com>
2017-04-28 12:44:04 -0400

    made progress geneaation more accurate

--- FetchData/main.js
+++ FetchData/main.js
@@ -1,6 +1,13 @@
 import scripturePaneData from './ScripturePane';
-import tNFetchData from './TranslationNotes'
+import tNFetchData from './TranslationNotes';
+let tWordsProgress = 0;
+let biblesProgress = 0;
 
+/**
+ * @description fetches all data that translationNotes needs.
+ * @param {object} props - props coming down from the core.
+ * @return {*} returns a promise that handles multiple data fetching.
+ */
 export default function fetchAllData(props) {
   const projectDetails = props.projectDetailsReducer;
   const bibles = props.resourcesReducer.bibles;
@@ -11,12 +18,31 @@ export default function fetchAllData(props) {
   const groupsIndexLoaded = props.groupsIndexReducer.loadedFromFileSystem;
 
   return new Promise(function(resolve, reject) {
-    scripturePaneData(projectDetails, bibles, actions, totalProgress, scripturePaneSettings)
+    scripturePaneData(projectDetails, bibles, actions, progress, scripturePaneSettings)
     .then(() => {
-      tNFetchData(projectDetails, bibles, actions, totalProgress, groupsIndexLoaded, groupsDataLoaded);
+      tNFetchData(projectDetails, bibles, actions, progress, groupsIndexLoaded, groupsDataLoaded);
     })
     .then(resolve).catch(e => {
       console.warn(e);
     });
   });
+
+  /**
+   * @description gets the multiple progress percentages from fethdatas and then puts them together.
+   * @param {string} label - fethdata name.
+   * @param {number} value - percenatage value.
+   */
+  function progress(label, value) {
+    switch (label) {
+      case "translationNotes":
+        tWordsProgress = value / 2;
+        break;
+      case "scripturePane":
+        biblesProgress = value / 2;
+        break;
+      default:
+        break;
+    }
+    totalProgress(tWordsProgress + biblesProgress);
+  }
 }
